dfaeac2b2dfca5a6fa08890be7d23efec79866aa
Maatari Okouya <d.okouya@elsevier.com>
2025-09-04 15:41:41 +0100

    tw1 done

--- tws/tws1.scala
+++ tws/tws1.scala
@@ -4,9 +4,10 @@ import org.apache.spark.sql.streaming.{OutputMode, StatefulProcessor, TimeMode, 
 import org.apache.spark.sql.functions.*
 import org.apache.spark.sql.streaming.*
 import org.apache.spark.sql.execution.streaming.MemoryStream
-
 import scribe.*
 import scribe.format.*
+
+import scala.util.chaining.scalaUtilChainingOps
 
 
 
@@ -20,10 +21,10 @@ object DataTypes:
     case class User(id: String, name: String, location: Location)
     case class UserByLocationAggregate(location: Location, users: List[User])
 
-    sealed abstract class Change[+T](val entity: T)
-    case class Insert[T](override val entity: T) extends Change[T](entity)
-    case class Update[T](override val entity: T) extends Change[T](entity)
-    case class Delete[T](override val entity: T) extends Change[T](entity)
+    // CDC aggregator phases with commit ordering
+    sealed abstract class Change[+T](val entity: T, val version: Long)
+    case class Add[T](override val entity: T, override val version: Long)    extends Change[T](entity, version)
+    case class Remove[T](override val entity: T, override val version: Long) extends Change[T](entity, version)
 
 
 
@@ -47,13 +48,25 @@ class UserByLocationProcessor extends StatefulProcessor[Location, Change[User], 
         timerValues: TimerValues
     ): Iterator[UserByLocationAggregate] =
 
-       val current  = if users.exists() then users.get() else List.empty[User]
-       val newUsers = inputRows map { case Insert(user) => user}
-       val next     = current.appendedAll(newUsers)
+
+       val inputRowsList = inputRows.toList
+
+       val current       = if users.exists() then users.get() else List.empty[User]
+
+       // Order by commit version, and within a version apply Remove before Add
+       val ordered       = inputRowsList.sortBy {
+         case Remove(_, v) => (v, 0)
+         case Add(_, v)    => (v, 1)
+       }
+
+        val next = ordered.foldLeft(current) {
+          case (acc, Remove(u, _)) => acc.filterNot(_.id == u.id)
+          case (acc, Add(u, _))    => acc.filterNot(_.id == u.id) :+ u
+        }
 
        users.update(next)
 
-       Iterator(UserByLocationAggregate(key, next))
+       Iterator.single(UserByLocationAggregate(key, next))
 
 
 
@@ -81,17 +94,66 @@ object TWS1:
 
         implicit val sqlCtx = spark.sqlContext
 
-        val userChangeDS = MemoryStream[Change[User]].toDS()
+        val memSrc = MemoryStream[Change[User]]
+        val userChangeDS = memSrc.toDS()
 
 
-        userChangeDS
-          .explain(true)
+        val aggregationQuery = userChangeDS
+          .groupByKey(_.entity.location)
+          .transformWithState[UserByLocationAggregate](
+              new UserByLocationProcessor,
+              TimeMode.EventTime,
+              OutputMode.Update
+          )
+          //.tap { _.explain(true) }
+          .writeStream
+          .outputMode(OutputMode.Update)
+          .format("console")
+          .option("truncate", value = false)
+          .start()
 
 
+        "1st micro-batch" pipe println
+        memSrc.addData(
+          Add(User("1", "U1", "Paris"),    1L),
+          Add(User("2", "U2", "Paris"),    1L),
+          Add(User("3", "U3", "London"),   1L),
+          Add(User("4", "U4", "Brazzaville"), 1L),
+          Add(User("5", "U5", "New York"), 1L),
+          Add(User("7", "Maatari", "Madrid"), 1L)
+        )
 
+        aggregationQuery.processAllAvailable()
 
+        "2nd micro-batch" pipe println
+        memSrc.addData(
+            Add(User("6", "U6", "New York"), 2L),
 
+            // Move id=1: Paris -> London within commit version 2
+            Remove(User("1", "U1", "Paris"),  2L),
+            Add(User("1", "U1", "London"),     2L),
 
+            // Move id=5: New York -> Brazzaville within commit version 2
+            Remove(User("5", "U5", "New York"), 2L),
+            Add(User("5", "U5", "Brazzaville"), 2L)
+        )
+
+        aggregationQuery.processAllAvailable()
+
+        "3rd micro-batch: Madrid -> Paris -> London" pipe println
+        memSrc.addData(
+          // Move 7: Madrid -> Paris at version 3
+          Remove(User("7", "Maatari", "Madrid"), 3L),
+          Add(User("7", "Maatari", "Paris"),     3L),
+
+          // Move 7: Paris -> London at version 4
+          Remove(User("7", "Maatari", "Paris"),  4L),
+          Add(User("7", "Maatari", "London"),    4L)
+        )
+        aggregationQuery.processAllAvailable()
+
+        // 6. Stop
+        spark.stop()
 
 
 
